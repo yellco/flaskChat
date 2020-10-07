810d7926c86191931ffad2d4b004c6fb0e8fa999
yellco <dazheltyy@gmail.com>
2020-10-07 22:24:04 +0300

    Add template styles

--- frontend/src/components/Enter.vue
+++ frontend/src/components/Enter.vue
@@ -1,88 +1,77 @@
 <template>
   <div>
-    <div class="container mt-5 mb-4">
-        <h1>Chat Room</h1>
-    </div>
     <div class="container">
-      <ul id="messages"></ul>
-        <div class="row">
-            <div class="col-7">
-              <div class="username">
-                <label for="message_input">Username:</label>
-                <input type="text" ref="username_input" v-model="username" placeholder="Enter your username">
-              </div>
-              <div class="message">
-                <label for="message_input">Message:</label>
-                <input type="text" ref="message_input" placeholder="Enter your message">
-                <button @click="sendMessage">Send</button>
-              </div>
-            </div>
+      <div ref="messages" id="messages">
+        <div class="message_block" v-for="(message, i) in messages" :key="message.msg + i">
+          <span class="username"><strong>{{message.username}}</strong></span>
+          <p class="message">{{message.msg}}</p>
         </div>
+      </div>
+      <div class="input_block">
+        <div class="input">
+          <div class="username_input">
+            <input type="text" ref="username_input" v-model="username" placeholder="Username...">
+          </div>
+          <div class="message_input">
+            <input type="text" ref="message_input" placeholder="Message...">
+          </div>
+        </div>
+        <div class="button" @click="sendMessage">></div>
+      </div>
     </div>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-import axios, { AxiosResponse } from 'axios'
-
-@Component({sockets:{
-      connect(){
-        console.log('connect was called');    
-      },
-      disconnect(){
-        console.log('disconnect was called');
-      },
-      message(data) {
-        console.log(data);
-        // TODO delete getElementById
-        document.getElementById("messages")!.append('<li class="text-muted"><strong>' + data.user + ':</strong>' + data.msg + '</li><br>')
+<script>
+export default {
+  name: 'Enter',
+  sockets: {
+    connect(){
+      console.log('connect was called');    
+    },
+    disconnect(){
+      console.log('disconnect was called');
+    },
+    message(data) {
+      console.log(typeof(data));
+      if (typeof(data) === 'object' && 'msg' in data) {
+        this.messages.push({
+          username: data.user,
+          msg: data.msg,
+        })
+        // this.$refs.messages.append(`<li class="text-muted"><strong>${data.user}:</strong>${data.msg}</li>`)
       }
-  }})
+    }
+  },
 
-export default class Enter extends Vue {
-  @Prop() private msg!: string;
+  data () {
+    return {
+      username: null,
+      host: "https://chat.yellco.ru",
+      messages: [],
+    }
+  },
 
-  private username = null;
-  private host = "https://chat.yellco.ru";
+  methods: {
+    sendMessage() {
+      if ((this.$refs.message_input.value).length !== 0) {
+        this.$socket.emit('chat-message', {
+          msg: this.$refs.message_input.value,
+          user: this.username
+        });
+        this.$refs.message_input.value = "";
+      }
+    },
+  },
 
-  // login() {
-  //   const data = new FormData();
-  //   data.append('username', (this.$refs.username as HTMLInputElement).value);
+  computed: {
+  },
 
-  //   axios
-  //     .post(this.host + '/chat/login', data)
-  //     .then(response => {
-  //         response.data === "ok" ? this.username = "" : null;
-  //       })    
-  // }
-
-  // logout() {
-  //   axios
-  //     .get(this.host + '/chat/logout')
-  //     .then(response => ( response.data === "ok" ? this.username = "" : null))
-  //   console.log(this.username);
-  // }
-
-  // getUsername() {
-  //   axios
-  //     .get(this.host + '/chat/username')
-  //     .then(response => (this.username = response.data))
-  //     .catch(error => console.log(error.response));    
-  // }
-
-  sendMessage() {
-    this.$socket.emit('chat-message', {
-      msg: (this.$refs.message_input as HTMLInputElement).value,
-      user: this.username
-    });
-    (this.$refs.message_input as HTMLInputElement).value = "";
+  mounted () {
+    // this.$socket.subscribe("kebab-case", function(data) {
+    //     console.log("This event was fired by eg. sio.emit('kebab-case')", data)
+    // })
   }
-
-  mounted() {
-    // this.getUsername();
-  }
-
 }
 </script>
 
@@ -101,5 +90,57 @@ li {
 }
 a {
   color: #42b983;
+}
+
+#messages{
+  padding-top: 10px;
+  padding-left: 10px;
+  padding-right: 10px;
+  height: calc(100vh - 90px - 78px - 30px);
+  max-height: calc(100vh - 90px - 78px - 30px);
+  overflow-y: auto;
+  background: #f5faff;
+
+  .message_block {
+    display: block;
+    border: 1px solid black;
+    width: 60%;
+    box-shadow: 0 1px 0 0 rgba(50,50,50,.3);
+    border-radius: 4px;
+    word-break: break-all;
+    margin: 0 5px 5px;
+    padding: 1rem;
+    text-align: left;
+    background: white;
+
+    .message {
+      margin: 0;
+      padding: 0;
+    }
+  }
+}
+
+.input_block {
+  display: flex;
+  padding: 10px;
+
+  .input {
+    width: 90%;
+    input {
+      width:100%;
+      padding: 10px;
+      font-size: 18px;
+      outline: none;
+      width: calc(100% - 23px);
+    }
+  }
+  .button {
+    width:10%;
+    cursor: pointer;
+    border: 1px solid black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 }
 </style>
